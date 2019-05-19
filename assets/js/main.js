ec898ca9ed4ae68c578638046d1a4cbc3391a055
leanneluz <47746762+leanneluz@users.noreply.github.com>
2019-05-19 11:58:36 +0100

    add js function

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,3 +121,25 @@
 				});
 
 })(jQuery);
+
+/* Leanne functions */
+
+function imagePopup(img, text) {
+	// Get the modal
+	var modal = document.getElementById("myModal");
+
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+			modal.style.display = "block";
+			modalImg.src = img.src;
+			captionText.innerHTML = text;
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+}
